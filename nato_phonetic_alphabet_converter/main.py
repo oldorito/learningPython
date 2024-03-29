47e9bd978cb53817e83e5490020fe4bfd8b36731
oladorito <ola.doroszuk@gmail.com>
2024-03-29 09:12:40 -0400

    nato-update

--- nato_phonetic_alphabet_converter/main.py
+++ nato_phonetic_alphabet_converter/main.py
@@ -1,9 +1,20 @@
 import pandas
+
 print("Extremely Epic NATO Alphabet converter!")
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters!")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
